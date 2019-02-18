e06e62d6040e0cd9aaac8806d79ef991fded35f1
Unknown <vaibhavkite3@gmail.com>
2019-02-18 21:47:21 +0530

    Commit to master branch

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-MQTTWiFiManager
+# MQTTWiFiManager
+ESP8266 MQTT and WiFi Connection manager with web configuration portal
+with FOTA capablity (See examples)
+
+Requirements:
+ - ArduinoJson Library https://arduinojson.org/?utm_source=meta&utm_medium=library.properties
+ - ESP8266OTA Library https://github.com/ChipTechno/ESP8266OTA
+
+This project forked from https://github.com/tzapu/WiFiManager origanl library maintained by tzapu.
+
+Added MQTT connection configuration options additional to wifi connection.
+For FOTA browse http://<your_board_ip>/update to update code
+
+UI tweaked and some options removed. 
+
+#### This works with the ESP8266 Arduino platform with a recent stable release(2.0.0 or newer) https://github.com/esp8266/Arduino
---
+++ examples/esp8266_ap_mqtt_wifi_config_with_FOTA/esp8266_ap_mqtt_wifi_config_with_FOTA.ino
@@ -0,0 +1,198 @@
+//include libraries
+#include <FS.h>                   
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>         
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <MQTTWiFiManager.h>          
+#include <ArduinoJson.h>
+#include <ESP8266OTA.h>
+
+//define your default values here, if there are different values in config.json, they are overwritten.
+char mqtt_server[40];
+char mqtt_port[6];
+char mqtt_username[50];
+char mqtt_password[50];
+char device_id[50];
+char publish_topic[100];
+char subscribe_topic[100];
+
+ESP8266WebServer httpServer(80);
+ESP8266OTA otaUpdater;
+
+//flag for saving data
+bool shouldSaveConfig = false;
+
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println();
+
+  //clean FS, for testing
+  //SPIFFS.format();
+
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+
+          strcpy(mqtt_server, json["mqtt_server"]);
+          strcpy(mqtt_port, json["mqtt_port"]);
+          strcpy(mqtt_username, json["mqtt_username"]);
+          strcpy(mqtt_password, json["mqtt_password"]);
+          strcpy(device_id, json["device_id"]);
+          strcpy(publish_topic, json["publish_topic"]);
+          strcpy(subscribe_topic, json["subscribe_topic"]);
+
+        } else {
+          Serial.println("failed to load json config");
+        }
+        configFile.close();
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+  //end read
+
+
+
+  // The extra parameters to be configured (can be either global or just in the setup)
+  // After connecting, parameter.getValue() will get you the configured value
+  // id/name placeholder/prompt default length
+  WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
+  WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 6);
+  WiFiManagerParameter custom_mqtt_username("mqtt_username", "mqtt username", mqtt_username, 50);
+  WiFiManagerParameter custom_mqtt_password("mqtt_password", "mqtt password", mqtt_password, 50);
+  WiFiManagerParameter custom_device_id("device_id", "device id", device_id, 50);
+  WiFiManagerParameter custom_publish_topic("publish_topic", "publish topic", publish_topic, 100);
+  WiFiManagerParameter custom_subscribe_topic("subscribe_topic", "subscribe topic", subscribe_topic, 100);
+  
+
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+  
+  //add all your parameters here
+  wifiManager.addParameter(&custom_mqtt_server);
+  wifiManager.addParameter(&custom_mqtt_port);
+  wifiManager.addParameter(&custom_mqtt_username);
+  wifiManager.addParameter(&custom_mqtt_password);
+  wifiManager.addParameter(&custom_device_id);
+  wifiManager.addParameter(&custom_publish_topic);
+  wifiManager.addParameter(&custom_subscribe_topic);
+
+  //reset settings - for testing
+  //wifiManager.resetSettings();
+
+  //set minimu quality of signal so it ignores AP's under that quality
+  //defaults to 8%
+  //wifiManager.setMinimumSignalQuality();
+  
+  //sets timeout until configuration portal gets turned off
+  //useful to make it all retry or go to sleep
+  //in seconds
+  //wifiManager.setTimeout(120);
+
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  if (!wifiManager.autoConnect("ESP8266-AP-CONFIG", "12345678")) {
+    Serial.println("failed to connect and hit timeout");
+    delay(3000);
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(5000);
+  }
+
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
+
+  //read updated parameters
+  strcpy(mqtt_server, custom_mqtt_server.getValue());
+  strcpy(mqtt_port, custom_mqtt_port.getValue());
+  strcpy(mqtt_username, custom_mqtt_username.getValue());
+  strcpy(mqtt_password, custom_mqtt_password.getValue());
+  strcpy(device_id, custom_device_id.getValue());
+  strcpy(publish_topic, custom_publish_topic.getValue());
+  strcpy(subscribe_topic, custom_subscribe_topic.getValue());
+
+  //save the custom parameters to FS
+  if (shouldSaveConfig) {
+    Serial.println("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["mqtt_server"] = mqtt_server;
+    json["mqtt_port"] = mqtt_port;
+    json["mqtt_username"] = mqtt_username;
+    json["mqtt_password"] = mqtt_password;
+    json["device_id"] = device_id;
+    json["publish_topic"] = publish_topic;
+    json["subscribe_topic"] = subscribe_topic;
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      Serial.println("failed to open config file for writing");
+    }
+
+    json.printTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    //end save
+  }
+
+  
+  //set web UI
+  otaUpdater.setUpdaterUi("FOTA","Firmware Update","Build : stable","Version : 1.0","OTA-Updater 1.0.0");
+
+  otaUpdater.setup(&httpServer);
+  httpServer.begin();
+
+  Serial.printf("HTTPUpdateServer ready! Open http://%s.local/update in your browser\n", device_id);
+
+  
+  Serial.println("local ip");
+  Serial.println(WiFi.localIP());
+
+  Serial.println(mqtt_server);
+  Serial.println(mqtt_port);
+  Serial.println(mqtt_username);
+  Serial.println(mqtt_password);
+  Serial.println(device_id);
+  Serial.println(publish_topic);
+  Serial.println(subscribe_topic);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  httpServer.handleClient();
+}
